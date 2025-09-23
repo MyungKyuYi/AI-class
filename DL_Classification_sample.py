70aa62d937d2e558a18a7c023b03c5391c862c6e
Myung-Kyu Yi <126849082+MyungKyuYi@users.noreply.github.com>
2025-09-23 11:47:19 +0900

    Update DL_Classification_sample.py

--- DL_Classification_sample.py
+++ DL_Classification_sample.py
@@ -1,3 +1,10 @@
+
+Y = pd.get_dummies(y).values
+X = X.values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test, y_train,y_test = train_test_split(X,Y,test_size=0.2,random_state=0) 
+
 # -------------------------------------------------
 # 5) 모델 정의 (작고 탄탄한 MLP)
 # -------------------------------------------------
@@ -10,11 +17,13 @@ x = tf.keras.layers.ReLU()(x)
 
 outputs = tf.keras.layers.Dense(1, activation="sigmoid")(x)
 model = tf.keras.Model(inputs, outputs)
+model.summary()
+
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
     loss="binary_crossentropy",
-    metrics=accuracy)
+    metrics=['accuracy'])
 
 model.summary()
 
@@ -23,9 +32,40 @@ model.summary()
 # -------------------------------------------------
 history = model.fit(
     train_ds,
-    validation_data=val_ds,
-    epochs=200,
-    class_weight=class_weight,
-    callbacks=callbacks,
-    verbose=2,
-)
+    validation_split=0.2,
+    epochs=20)
+
+
+y_pred = model.predict(X_test)
+
+y_test_class = np.argmax(y_test,axis=1)
+y_pred_class = np.argmax(y_pred,axis=1)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+loss =model_history.history['loss']
+val_loss =model_history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc =model_history.history['accuracy']
+val_acc =model_history.history['val_accuracy']
+plt.plot(epochs, acc, 'y', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+#Accuracy of the predicted values
+from sklearn.metrics import classification_report,confusion_matrix
+print(classification_report(y_test_class,y_pred_class))
+print(confusion_matrix(y_test_class,y_pred_class))
